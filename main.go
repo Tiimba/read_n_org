f1fa7cd01f57b39f4c4c693aedcc29c05fffa964
Timba <thiagodesouza07t@gmail.com>
2023-03-07 01:02:04 -0300

    app build

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
-import "fmt"
-
-/*
-TO_DO:
-Escreva um programa que leia um arquivo de texto e exiba o número de ocorrências de cada palavra no arquivo.
-Ignore a diferença entre maiúsculas e minúsculas e a pontuação.
-O programa deve imprimir as palavras e o número de ocorrências em ordem alfabética.
-*/
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+)
 
 func main() {
-	fmt.Println("PlaceHolder")
+	userPath := os.Args[1]
+	file, err := os.Open(userPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	wordCount := make(map[string]int)
+	re := regexp.MustCompile("[^a-zA-Z ]+")
+
+	for scanner.Scan() {
+		line := strings.ToLower(scanner.Text())
+		line = re.ReplaceAllString(line, "")
+
+		words := strings.Split(line, " ")
+		for _, word := range words {
+			if word != "" {
+				wordCount[word]++
+			}
+		}
+	}
+	pairs := make([][2]interface{}, 0, len(wordCount))
+	for word, count := range wordCount {
+		pairs = append(pairs, [2]interface{}{word, count})
+	}
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i][0].(string) < pairs[j][0].(string)
+	})
+	for _, pair := range pairs {
+		fmt.Printf("%s: %d\n", pair[0].(string), pair[1].(int))
+	}
 }
