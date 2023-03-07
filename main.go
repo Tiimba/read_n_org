b5206672623ae0eabfc87867cc108f78f50f5284
Timba <thiagodesouza07t@gmail.com>
2023-03-07 00:24:21 -0300

    Inital

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+/*
+TO_DO:
+Escreva um programa que leia um arquivo de texto e exiba o número de ocorrências de cada palavra no arquivo.
+Ignore a diferença entre maiúsculas e minúsculas e a pontuação.
+O programa deve imprimir as palavras e o número de ocorrências em ordem alfabética.
+*/
+
+func main() {
+	fmt.Println("PlaceHolder")
+}
